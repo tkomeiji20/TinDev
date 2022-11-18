b5599ac88b3c2fcb948da0907f57e9c2beb30338
tkomeiji20 <54751850+tkomeiji20@users.noreply.github.com>
2022-11-18 15:22:55 -0500

    Initial Post Query Logic

--- src/TinDev/posts/views.py
+++ src/TinDev/posts/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import PostForm
+from .models import Post
 
 
 # Create your views here.
 def create(request):
+    # Render the dashboard or create the post depending on request type
     if request.method == 'GET':
         form = PostForm()
         return render(request, 'Posts/create.html', {'form': form})
     elif request.method == 'POST':
         form = PostForm(request.POST)
+        # TODO: Add a check to see valid permissions
+        # Check: https://docs.djangoproject.com/en/4.1/topics/auth/default/
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/user/dashboard')
 
     form = PostForm()
     return render(request, 'Posts/create.html', {'form': form})
+
+
+def getPosts(id=-1):
+    '''Gets specific post id, else returns all posts'''
+    if id < 0:
+        posts = Post.objects.all()
+        return posts
+
+    try:
+        return Post.objects.get(pk=id)
+    except ObjectDoesNotExist:
+        return None
+
 
 
 def index(request):
